ef592e77cc6559c5d080cb44bab38a691222157b
xedgeuser <ksuzuki@dl-techx-xedge.net>
2024-09-03 11:25:13 +0900

    プロジェクト ファイルを追加します。

---
+++ Class.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace FunctionAPIApp
+{
+    [JsonObject]
+    public class Task_NewTableList
+    {
+        [JsonProperty("List")]
+        public List<Task_NewTableRow> List { get; set; } = new List<Task_NewTableRow>();
+    }
+
+    [JsonObject]
+    public class Task_NewTableRow
+    {
+        [JsonProperty("TaskID")]
+        public int TaskID { get; set; }
+
+        [JsonProperty("Title")]
+        public string Title { get; set; }
+
+        [JsonProperty("Status")]
+        public string Status { get; set; }
+
+        [JsonProperty("DueDate")]
+        public DateTime DueDate { get; set; }
+    }
+}
