e14b30fcc991ab0cbb91fbd120cb13d66d1d85b1
unknown <ksuzuki@dl-techx-xedge.net>
2024-09-03 16:08:48 +0900

    コミット

---
+++ employee_table.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace recipeApp2
+{
+    [JsonObject]
+    public class employee_tableList
+    {
+        [JsonProperty("List")]
+        public List<employee_tableRow> List { get; set; } = new List<employee_tableRow>();
+    }
+
+    [JsonObject]
+    public class employee_tableRow
+    {
+        [JsonProperty("employee_id")]
+        public int employee_id { get; set; }
+
+        [JsonProperty("employee_password")]
+        public string employee_password { get; set; }
+
+       
+
+
+    }
+}
