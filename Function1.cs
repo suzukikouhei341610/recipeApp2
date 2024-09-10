1e55fce3cd491ec0a3fa0753b03d8ef4d07bca15
matsumomona <mmatsumoto@dl-techx-xedge.net>
2024-09-10 10:18:38 +0900

    commit

--- Function1.cs
+++ Function1.cs
@@ -78,6 +78,7 @@ namespace FunctionAPIApp
                                 // 認証成功時にトークンを生成
                                 var tokenService = new TokenService();
                                 string token = tokenService.GenerateToken(user_name);
+                                log.LogInformation($"Generated token: {token}");
                                 return new OkObjectResult(new { token = token });
                                 //string token = GenerateToken(user_name); // トークン生成のロジックを実装
                                 //return new OkObjectResult(new { token = token });
